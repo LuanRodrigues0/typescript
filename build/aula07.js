a33057e7e72a414818aa9ece3da9649472a078d0
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2025-01-15 19:00:48 -0300

    Tuplas conceitos básicos

--- build/aula07.js
+++ build/aula07.js
@@ -1,1 +1,6 @@
 "use strict";
+let coisas = ["Minecraft", 100, true];
+coisas.push("Bomba", 23, true);
+coisas[2] = false;
+console.log(coisas);
+console.log(coisas[2]);
--- src/aula07.ts
+++ src/aula07.ts
@@ -1,1 +1,10 @@
 //Curso de Typescript Completo #P07 Aprendendo sobre TUPLAS em Typescript
+
+//Conceite de Tuplas conciste em especificar os tipos de dados para cada posição.
+
+let coisas:[string,number,boolean]=["Minecraft",100,true];
+coisas.push("Bomba", 23, true)
+coisas[2]=false;
+
+console.log(coisas)
+console.log(coisas[2])
