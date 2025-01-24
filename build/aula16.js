a842af220e03e311c8f3866a0042759ec41d7c8a
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2025-01-24 20:00:50 -0300

    class

---
+++ build/aula16.js
@@ -0,0 +1,16 @@
+"use strict";
+class computador {
+    nome = 'pc 1';
+    ram = 0;
+    cpu = 0;
+    Ligado = false;
+}
+const d1 = new computador;
+const d2 = new computador;
+const d3 = new computador;
+d1.nome = "games";
+d2.nome = "rápido";
+d3.nome = "Lesma";
+console.log(d1.nome);
+console.log(d2.nome);
+console.log(d3.nome);
